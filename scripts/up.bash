88fc0d86c9e67976a43653e28ac6bc1efb951526
Isaac Halvorson <hello@hisaac.net>
2024-11-12 09:22:52 -0600

    wip

--- scripts/up.bash
+++ scripts/up.bash
@@ -5,7 +5,7 @@ trap 'exit_handler "$?" "${0##*/}"' EXIT
 
 function main() {
 	if [[ -f "${MISE_BIN}" ]]; then
-		update_mise
+		maybe_update_mise
 	else
 		install_mise
 	fi
@@ -25,9 +25,32 @@ function install_mise() {
 	eval "$("${MISE_BIN}" activate -C "$PROJECT_ROOT" bash --shims)"
 }
 
+function maybe_update_mise() {
+	if [[ -z "${MISE_UPDATE_CHECK:-}" ]]; then
+		update_mise
+		return
+	fi
+
+	declare -ri epoch_time="$(date +%s)"
+	declare -ri seconds_since_mise_update="$((epoch_time - MISE_UPDATE_CHECK))"
+	declare -ri one_day_in_seconds=86400
+
+	if [[ "${seconds_since_mise_update}" -gt "${one_day_in_seconds}" ]]; then
+		update_mise
+	fi
+}
+
 function update_mise() {
 	info "Updating mise"
 	"${MISE_BIN}" self-update --yes || true # Ignore errors if mise is already up-to-date
+
+	# Update the timestamp in the .env file
+	declare -ri epoch_time="$(date +%s)"
+	if grep -q "^MISE_UPDATE_CHECK=" "${ENV_FILE}"; then
+		sed -i '' "s/^MISE_UPDATE_CHECK=.*/MISE_UPDATE_CHECK=${epoch_time}/" "${ENV_FILE}"
+	else
+		echo "MISE_UPDATE_CHECK=${epoch_time}" >> "${ENV_FILE}"
+	fi
 }
 
 function install_plugins() {
